fccfe8c920b5759f6a0e3eac5a5b4b6740e0e0cf
Олег <oleg@MacBook-Pro-Oleg.local>
2023-10-01 14:40:30 +0300

    iter1: have been created simple http server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,69 @@
 package main
 
-func main() {}
+import (
+	"encoding/base64"
+	"io"
+	"log"
+	"net/http"
+)
+
+var linkStorage = map[string]string{}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", requestHandler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		createLink(w, r)
+
+	case http.MethodGet:
+		getLink(w, r)
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func createLink(w http.ResponseWriter, r *http.Request) {
+	rData, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(r.URL.Path[1:]) != 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	linkStorage[base64.RawStdEncoding.EncodeToString(rData)] = string(rData)
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(linkStorage[base64.RawStdEncoding.EncodeToString(rData)]))
+
+	return
+}
+
+func getLink(w http.ResponseWriter, r *http.Request) {
+	if len(r.URL.Path) > 1 {
+		link, ok := linkStorage[r.URL.Path[1:]]
+
+		if ok {
+			w.Header().Set("Location", link)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+			return
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+}
