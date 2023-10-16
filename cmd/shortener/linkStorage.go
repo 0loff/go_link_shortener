a428871d3060b9fcff1990d5564f390475a4b9c2
Олег <oleg@MacBook-Pro-Oleg.local>
2023-10-16 06:16:36 +0300

    iter5: Added linkStorage locking via Mutex

--- cmd/shortener/linkStorage.go
+++ cmd/shortener/linkStorage.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"log"
+	"sync"
 )
 
 var storage LinkStorageRepository
 
 type LinkStorageRepository struct {
+	mu          sync.Mutex
 	linkEntries map[string]string
 }
 
-func (ls LinkStorageRepository) LinkStorageCreate() LinkStorageRepository {
+func (ls *LinkStorageRepository) LinkStorageCreate() {
 	ls.linkEntries = make(map[string]string)
-	return ls
 }
 
-func (ls LinkStorageRepository) FindByID(id string) (string, bool) {
+func (ls *LinkStorageRepository) FindByID(id string) (string, bool) {
+	ls.mu.Lock()
+	defer ls.mu.Unlock()
 	entry, ok := ls.linkEntries[id]
 	return entry, ok
 }
 
-func (ls LinkStorageRepository) FindByLink(link string) string {
+func (ls *LinkStorageRepository) FindByLink(link string) string {
+	ls.mu.Lock()
+	defer ls.mu.Unlock()
 	for index, value := range ls.linkEntries {
 		if link == value {
 			return index
@@ -30,21 +35,23 @@ func (ls LinkStorageRepository) FindByLink(link string) string {
 	return ""
 }
 
-func (ls LinkStorageRepository) SetShortLink(shortURL string, originURL string) LinkStorageRepository {
+func (ls *LinkStorageRepository) SetShortLink(shortURL string, originURL string) {
+	ls.mu.Lock()
 	ls.linkEntries[shortURL] = originURL
-	return ls
+	ls.mu.Unlock()
 }
 
-func (ls LinkStorageRepository) GetShortLink(shortURL string) (string, bool) {
+func (ls *LinkStorageRepository) GetShortLink(shortURL string) (string, bool) {
 	entry, ok := ls.FindByID(shortURL)
 	return entry, ok
 }
 
-func (ls LinkStorageRepository) LogAllEntries() {
+func (ls *LinkStorageRepository) LogAllEntries() {
 	log.Println(ls)
 }
 
 func LinkStorageInit() {
-	storage = new(LinkStorageRepository).LinkStorageCreate()
+	storage = LinkStorageRepository{}
+	storage.LinkStorageCreate()
 	storage.SetShortLink("OL0ZGlVC3dq", "https://practicum.yandex.ru/")
 }
